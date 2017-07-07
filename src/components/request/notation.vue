1031b052cc435f67bd2f07a59513a96fe19cfb38
Roman <vizvoxer@gmail.com>
2017-07-07 09:07:16 +0300

    sfgsrgsrgsrg

---
+++ src/components/request/notation.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="notation-request-wrapper">
+        <p>Вы можете создать обьявление о поиске доноров и поиск будет вестить в автоматическом режиме. Для этого заполните все необходимые поля ниже и нажмите кнопку - "Создать заявку"</p>
+        <p>Система подберет доноров, подходящих под необходимые условия, чтобы вы могли связаться с ними, а так же оповестит о вашем обьявлении часть доноров по доступным каналам связи.</p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+    .notation-request-wrapper{
+        background-color: #0065BD;
+        color:#fff;
+        padding: 10px;
+             background-image: url("~../assets/background.jpg");
+  background-size: auto;
+  background-repeat:repeat;
+    }
+</style>
